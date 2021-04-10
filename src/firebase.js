80ab14cb517dc33db8718615523bc1982165c2b3
ashish-pinaak <ashish.sahrawat2@gmail.com>
2021-04-11 04:34:36 +0530

    changes

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,8 @@
 import firebase from 'firebase';
 
-const firebaseApp=firebase.initializeApp({
-    apiKey: "AIzaSyCMfQFZe7SP8vu9uRfY9qb9s7T7-bPXxcA",
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyCMfQFZe7SP8vu9uRfY9qb9s7T7-bPXxcA",
   authDomain: "todo-react-a54af.firebaseapp.com",
   projectId: "todo-react-a54af",
   storageBucket: "todo-react-a54af.appspot.com",
@@ -9,7 +10,7 @@ const firebaseApp=firebase.initializeApp({
   appId: "1:689497709448:web:b20182e9005014e9a7f83d"
 });
 
-const db=firebaseApp.firestore();
+const db = firebaseApp.firestore();
 
 export default db;
 //module.exports={db};
